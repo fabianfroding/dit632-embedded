74b607ad9002b6f84616a69b7c2e8c8cdbd082ea
Trooper98 <oliver.g.ryan@gmail.com>
2019-02-07 16:45:56 +0100

    completely removed the second level in the wp2 folder

--- wp2/exerc_2_2.c
+++ wp2/exerc_2_2.c
@@ -11,7 +11,6 @@ int main()
     //defining arr
     int arr[MAX];
     void *pointer = arr;
-    int arrSize = 0;
     int aInteger;
 
     //populating arr
@@ -21,12 +20,11 @@ int main()
     for (i = 0; i < MAX; i++)
     {
         arr[i] = (rand() % 99 + 1); //from 1 to 99
-        arrSize++;
     }
 
     int firstValue = arr[0];
     printf("The value of the label array (address) is: %p\n", &arr);                  //address
     printf("The first integer in the array is (array[0]): %d\n", firstValue);         //first value
     printf("The size of an integer (number of bytes) is: %lu\n", sizeof(firstValue)); //size of first value
-    printf("The size of the whole array is: %d\n", arrSize - 1);                      //array size
+    printf("The size of the whole array is: %d\n", sizeof(arr));                      //array size
 }
