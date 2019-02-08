d67ce72271fabdfbe76ea2c85e479d1a08abae50
fabianfroding <fabian.froding@gmail.com>
2019-02-08 15:24:58 +0100

    Fix 2.1

--- wp2/exerc_2_1.c
+++ wp2/exerc_2_1.c
@@ -11,7 +11,6 @@ Demonstration code: [none]
 #include <string.h>
 
 #define MAX 20
-#define LOW 5
 
 void copyString(char *str1, char *str2) {
     int i = 0;
@@ -22,36 +21,22 @@ void copyString(char *str1, char *str2) {
     *str2 = '\0';
 }
 
-int doesFileExist(const char *filename){
-    FILE *file;
-    if (file = fopen(filename, "r")) {
-        fclose(file);
-        return 1;
-    }
-    return 0;
-}
-
 int main(int argc, char *argv[]) {
-    if (argc == 2) {
-        printf("Received: %s\n", argv[1]);
-        char str1[MAX];
-        char str2[MAX];
-
-        if (doesFileExist(argv[1])) {
-            FILE* fPointer = fopen(argv[1], "r"); // "r" for read.
-
-            fgets(str1, MAX + 1, fPointer); // +1. Even though MAX is set to 20, only counts 19 from .txt file.
-
-            copyString(str1, str2);
-            fclose(fPointer);
-        } else {
-            copyString(argv[1], str2);
-        }
-        printf("Copied string: %s", str2);
+    char input[MAX];
+    char copiedString[MAX];
+    if (argc == 1) {
+        fgets(input, MAX, stdin);
+        printf("Input: %s", input);
+        copyString(input, copiedString);
+    }
+    else if (argc == 2) {
+        printf("Input: %s\n", argv[1]);
+        copyString(argv[1], copiedString);
     }
     else {
-        printf("Expected 1 argument, received: %d.\n", argc);
+        printf("Expected 1 or 2 arguments, received: %d.\n", argc);
     }
+    printf("Copied string: %s", copiedString);
 
     return 0;
 }
