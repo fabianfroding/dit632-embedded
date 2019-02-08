bfbba45a70484ac9122695db331a57e82bcdf136
Trooper98 <oliver.g.ryan@gmail.com>
2019-02-08 13:09:31 +0100

    added solution for ex6

--- wp2/exerc_2_2.c
+++ wp2/exerc_2_2.c
@@ -10,7 +10,7 @@ int main()
 {
     //defining arr
     int arr[MAX];
-    void *pointer = arr;
+    int *pointer = &arr[0];
     int aInteger;
 
     //populating arr
@@ -23,8 +23,16 @@ int main()
     }
 
     int firstValue = arr[0];
-    printf("The value of the label array (address) is: %p\n", &arr);                  //address
-    printf("The first integer in the array is (array[0]): %d\n", firstValue);         //first value
+    printf("The value of the label array (address) is: %p\n\n", *pointer);            //address
+    printf("The first integer in the array is: %d\n", firstValue);                    //first value
     printf("The size of an integer (number of bytes) is: %lu\n", sizeof(firstValue)); //size of first value
     printf("The size of the whole array is: %lu\n", sizeof(arr));                     //array size
+
+    printf("printing array values\n");
+    int arrLength = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < arrLength; i++)
+    {
+        printf("[index %d] [value %d] [value doubled %d]\n", i, *pointer, (*pointer * 2));
+        pointer++;
+    }
 }
--- wp2/exerc_2_6.c
+++ wp2/exerc_2_6.c
@@ -7,11 +7,6 @@
 
 #define MAX 5
 
-int main()
-{
-    int que[MAX];
-}
-
 void initQue(int list[], int max)
 {
     for (int i = 0; i < max; i++)
@@ -22,24 +17,100 @@ void initQue(int list[], int max)
 
 int input(int list[], int number, int max)
 {
-    int arrLength = sizeof(list) / sizeof(int);
+    int arrLength = sizeof(list) / sizeof(list[0]);
     int isFull = false; //boolean
     int success = 1, fail = 0;
-    for (int i = 0; i < arrLength; i++)
+    int curr = 0, next = curr + 1;
+    while (curr < MAX)
     {
-        /* code */
-        if (list[i] == -1)
+        if (list[curr] == -1)
         {
-            list[i] = number;
+            list[curr] = number;
             return success;
-        }
-        else if ((i + 1) == arrLength && list[i] != -1)
-        {
-            return fail;
         }
         else
         {
-            return fail;
+            curr++;
         }
     }
+    return fail;
+}
+
+int output(int list[], int max)
+{ //assumption: this is a "pop" method
+    int arrLength = MAX;
+    if (isEmpty(list) == true)
+    {
+        return 0;
+    }
+    else
+    {
+        int value = list[0];
+        int curr = 0, next = curr + 1;
+        int empty = -1;
+        while (curr < arrLength)
+        {
+            printf("current index before change --> %d current value --> %d\n", curr, list[curr]);
+            if (next == arrLength)
+            {
+                list[curr] = empty;
+            }
+            else if (list[next] == empty)
+            {
+                //if next is empty
+                list[curr] = empty;
+            }
+            else
+            {
+                list[curr] = list[next];
+            }
+            printf("current index after change --> %d new value --> %d\n", curr, list[curr]);
+            curr++;
+            next = curr + 1;
+        }
+        return value;
+    }
+}
+
+int isEmpty(int list[])
+{
+    int empty = -1, isEmpty = 1, notEmpty = 0;
+    if (list[0] == empty)
+    {
+        return isEmpty;
+    }
+    else
+    {
+        return notEmpty;
+    }
+}
+
+void printList(int list[])
+{
+    // int arrLength = sizeof(list) / sizeof(list[0]);
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("[%d]", list[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int que[MAX];
+    initQue(que, MAX);
+    printf("initialized list\n");
+    printList(que);
+    int inputState = input(que, 3, MAX);
+    printf("input value %d | input state: %d\n", 3, inputState);
+    printList(que);
+    inputState = input(que, 4, MAX);
+    printf("input value %d | input state: %d\n", 4, inputState);
+    printList(que);
+    inputState = input(que, 7, MAX);
+    printf("input value %d | input state: %d\n", 7, inputState);
+    printList(que);
+    int outputState = output(que, MAX);
+    printf("output state: %d\n", outputState);
+    printList(que);
 }
