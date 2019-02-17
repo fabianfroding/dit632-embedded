6a625dc9cff8c74e715c5b16cccf19813d18e9b7
Trooper98 <oliver.g.ryan@gmail.com>
2019-02-17 20:10:08 +0100

    prepped work package 4

---
+++ wp4/exerc_4_1.c
@@ -0,0 +1,9 @@
+/* ==================================== 
+File name: exerc_4_1.c
+Date: 2019-03-01
+Group Number:02
+Members that contributed:
+Oliver
+Fabian
+Demonstration code: [None] 
+====================================== */
---
+++ wp4/exerc_4_5.c
@@ -0,0 +1,9 @@
+/* ==================================== 
+File name: exerc_4_5.c
+Date: 2019-03-01
+Group Number:02
+Members that contributed:
+Oliver
+Fabian
+Demonstration code: [None] 
+====================================== */
